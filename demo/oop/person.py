b665edc971ab8ddafe713ec92db3225fc841d8eb
srikanthpragada <srikanthpragada@gmail.com>
2020-04-20 18:56:19 +0530

    20-APR

---
+++ demo/oop/person.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} - {self.age}"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __gt__(self, other):
+        print(self,other)
+        return self.age > other.age
+
+
+p1 = Person("Scott", 25)
+p2 = Person("Tom", 35)
+print(p1 > p2)
+print(p1 < p2)
+p3 = Person("Scott", 25)
+print(p1 == p3)
+print(p1 != p2)
+
+print(p1)
+
+
--- demo/oop/product.py
+++ demo/oop/product.py
@@ -1,4 +1,17 @@
 class Product:
+    # class attributes
+    taxrate = 15
+
+    @staticmethod
+    def set_taxrate(taxrate):
+        if taxrate >= 0:
+            Product.taxrate = taxrate
+
+    # Create a dummy product object
+    @classmethod
+    def create(cls, name):
+        return cls(name)
+
     # Constructor
     def __init__(self, name, price=0):
         # Object Attributes
@@ -7,9 +20,16 @@ class Product:
 
     # Methods
     def net_price(self):
-        return self.price * 1.15
+        return self.price + self.price * Product.taxrate / 100
 
+
+p = Product.create("Dummy")  # Call classmethod
+print(p.name, p.price)
 
 p1 = Product("Bose HeadPhones", 25000)
-p2 = Product("iPhone 11")
+# Product.taxrate = 20
+Product.set_taxrate(17)
 print(p1.net_price())
+print(p1.__dict__)
+
+p2 = Product("iPhone 11")
